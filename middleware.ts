3fb4ba16ca4a153c2c561751a71f3b2a471545d3
NexaHEAD <Sulafarafeeqk@email.com>
2026-03-28 14:05:54 +0400

    fix type error

--- middleware.ts
+++ middleware.ts
@@ -12,10 +12,7 @@ export async function middleware(request: NextRequest) {
         getAll() {
           return request.cookies.getAll()
         },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value }) =>
-            request.cookies.set(name, value)
-          )
+        setAll(cookiesToSet: { name: string; value: string; options?: any }[]) {
           supabaseResponse = NextResponse.next({ request })
           cookiesToSet.forEach(({ name, value, options }) =>
             supabaseResponse.cookies.set(name, value, options)
